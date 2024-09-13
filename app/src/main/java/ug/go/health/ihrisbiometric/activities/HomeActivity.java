91cf33dfc11df8fc7e9025b55b6541f8e966f0ac
Paul Obunga <paulobunga.one@gmail.com>
2024-09-13 20:17:25 +0300

    feat: add TextView and SwitchCompat imports to HomeActivity

--- app/src/main/java/ug/go/health/ihrisbiometric/activities/HomeActivity.java
+++ app/src/main/java/ug/go/health/ihrisbiometric/activities/HomeActivity.java
@@ -10,10 +10,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
