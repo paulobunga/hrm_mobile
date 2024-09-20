87e059ffd4bc9833cd56c3a13fc011e80abe920d
Paul Obunga <paulobunga.one@gmail.com>
2024-09-20 18:03:46 +0300

    feat: add Size import in CameraFragment for camera preview size handling

--- app/src/main/java/ug/go/health/ihrisbiometric/fragments/CameraFragment.java
+++ app/src/main/java/ug/go/health/ihrisbiometric/fragments/CameraFragment.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
