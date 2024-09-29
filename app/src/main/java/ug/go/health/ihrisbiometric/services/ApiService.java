8f8f023a07bb44eceb72c5fecd3cc1c77fa02de4
Paul Obunga <paulobunga.one@gmail.com>
2024-09-29 19:05:31 +0300

    refactor: simplify ApiService and improve caching logic

--- app/src/main/java/ug/go/health/ihrisbiometric/services/ApiService.java
+++ app/src/main/java/ug/go/health/ihrisbiometric/services/ApiService.java
@@ -3,16 +3,9 @@ package ug.go.health.ihrisbiometric.services;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
-
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import android.util.Log;
 
 import java.io.File;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
@@ -21,129 +14,140 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import io.sentry.okhttp.SentryOkHttpInterceptor;
-import io.sentry.okhttp.SentryOkHttpEventListener;
 import okhttp3.logging.HttpLoggingInterceptor;
-import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import ug.go.health.ihrisbiometric.models.DeviceSettings;
 
 public class ApiService {
 
-    private static final String SECRET_KEY = "qwerty@123";
-    //    private static final String ISSUER = "your_issuer_here";
-    private static final int TOKEN_EXPIRATION_TIME = 3600 * 24; // 24 hours
+    private static final String TAG = ApiService.class.getSimpleName();
+    private static final int CACHE_SIZE = 10 * 1024 * 1024; // 10 MB
+    private static final int MAX_AGE = 60; // 1 minute
+    private static final int MAX_STALE = 7 * 24 * 60 * 60; // 7 days
 
     private static ApiInterface apiInterface;
+    private static OkHttpClient okHttpClient;
+    private static TokenInterceptor tokenInterceptor;
 
-    private static DbService dbService;
+    private ApiService() {
+        // Private constructor to prevent instantiation
+    }
 
     public static ApiInterface getApiInterface(Context context, String token) {
-        // Define cache size and cache directory
-        int cacheSize = 10 * 1024 * 1024; // 10 MB
-        File cacheDirectory = new File(context.getCacheDir(), "api_cache");
-
-        // Create cache object
-        Cache cache = new Cache(cacheDirectory, cacheSize);
-
-        // Create logging interceptor
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(Level.BODY);
-
-        // Create network interceptor
-        Interceptor networkInterceptor = chain -> {
-            Request request = chain.request();
-            if (!ApiService.isInternetAvailable(context)) {
-                CacheControl cacheControl = new CacheControl.Builder()
-                        .maxStale(7, TimeUnit.DAYS)
-                        .build();
-                request = request.newBuilder()
-                        .cacheControl(cacheControl)
-                        .build();
+        if (apiInterface == null) {
+            synchronized (ApiService.class) {
+                if (apiInterface == null) {
+                    apiInterface = createApiInterface(context);
+                }
             }
+        }
+        return apiInterface;
+    }
 
-            // Add "Accept: application/json" header to RequestBuilder object
-            request = request.newBuilder()
-                    .addHeader("Accept", "application/json")
-                    .addHeader("Content-Type", "application/json")
-                    .build();
+    private static ApiInterface createApiInterface(Context context) {
+        okHttpClient = buildOkHttpClient(context);
+        String baseUrl = getBaseUrl(context);
 
-            // Add token to RequestBuilder object if it is not null
-            if (token != null) {
-                request = request.newBuilder()
-                        .addHeader("Authorization", "Bearer " + token)
-                        .build();
-            } else {
-                Log.w(TAG, "Token is null, Authorization header not set");
-            }
-
-            Response response = chain.proceed(request);
-            if (ApiService.isInternetAvailable(context)) {
-                int maxAge = 60; // 1 minute
-                response.newBuilder()
-                        .header("Cache-Control", "public, max-age=" + maxAge)
-                        .build();
-            } else {
-                int maxStale = 7 * 24 * 60 * 60; // 7 days
-                response.newBuilder()
-                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
-                        .build();
-            }
-
-
-            return response;
-        };
-
-        loggingInterceptor.setLevel(Level.BODY);
-
-        // Create OkHttpClient with cache, network interceptors, and Sentry logging
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .cache(cache)
-                .addInterceptor(loggingInterceptor)
-                .addNetworkInterceptor(networkInterceptor)
-                .addInterceptor(new SentryOkHttpInterceptor())
-                .eventListener(new SentryOkHttpEventListener())
-                .build();
-
-        // Get base url from device settings
-        SessionService session = new SessionService(context);
-        DeviceSettings deviceSettings = session.getDeviceSettings();
-        String baseUrl = deviceSettings.getServerUrl();
-
-        // Create Retrofit object
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClient)
                 .build();
 
-        // Create ApiInterface object
-        apiInterface = retrofit.create(ApiInterface.class);
-
-        return apiInterface;
+        return retrofit.create(ApiInterface.class);
     }
 
-    // Check if internet is available
+    private static OkHttpClient buildOkHttpClient(Context context) {
+        File cacheDirectory = new File(context.getCacheDir(), "api_cache");
+        Cache cache = new Cache(cacheDirectory, CACHE_SIZE);
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        tokenInterceptor = new TokenInterceptor();
+
+        return new OkHttpClient.Builder()
+                .cache(cache)
+                .addInterceptor(loggingInterceptor)
+                .addInterceptor(tokenInterceptor)
+                .addNetworkInterceptor(provideNetworkInterceptor(context))
+                .build();
+    }
+
+    private static Interceptor provideNetworkInterceptor(Context context) {
+        return chain -> {
+            Request originalRequest = chain.request();
+            Request.Builder requestBuilder = originalRequest.newBuilder()
+                    .addHeader("Accept", "application/json")
+                    .addHeader("Content-Type", "application/json");
+
+            if (!isInternetAvailable(context)) {
+                requestBuilder.cacheControl(new CacheControl.Builder()
+                        .maxStale(MAX_STALE, TimeUnit.SECONDS)
+                        .build());
+            }
+
+            Request request = requestBuilder.build();
+            Response response = chain.proceed(request);
+
+            if (isInternetAvailable(context)) {
+                response = response.newBuilder()
+                        .header("Cache-Control", "public, max-age=" + MAX_AGE)
+                        .build();
+            } else {
+                response = response.newBuilder()
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + MAX_STALE)
+                        .build();
+            }
+
+            return response;
+        };
+    }
+
+    private static String getBaseUrl(Context context) {
+        SessionService session = new SessionService(context);
+        DeviceSettings deviceSettings = session.getDeviceSettings();
+        return deviceSettings.getServerUrl();
+    }
+
     public static boolean isInternetAvailable(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connectivityManager != null) {
             NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
-            if (capabilities != null) {
-                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    return true;
-                }
-            }
+            return capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                    || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI));
         }
         return false;
     }
 
-    // Decode the JWT token and return the payload
-    public static Map<String, Claim> decodeToken(String token) throws JWTDecodeException {
-        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-        JWTVerifier verifier = JWT.require(algorithm)
-                .build();
-        DecodedJWT jwt = verifier.verify(token);
-        return jwt.getClaims();
+    public static void setToken(String token) {
+        if (tokenInterceptor != null) {
+            tokenInterceptor.setToken(token);
+        } else {
+            Log.e(TAG, "TokenInterceptor is null. Make sure ApiInterface is initialized before setting the token.");
+        }
+    }
+
+    private static class TokenInterceptor implements Interceptor {
+        private String token;
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+        @Override
+        public Response intercept(Chain chain) throws java.io.IOException {
+            Request original = chain.request();
+            if (token == null || token.isEmpty()) {
+                return chain.proceed(original);
+            }
+
+            Request.Builder requestBuilder = original.newBuilder()
+                    .header("Authorization", "Bearer " + token);
+
+            Request request = requestBuilder.build();
+            return chain.proceed(request);
+        }
     }
 }
