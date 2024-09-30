43104dd20094a7912b396c90c97f4918bc588fc5
Paul Obunga <paulobunga.one@gmail.com>
2024-09-30 13:23:49 +0300

    feat: add ArrayAdapter and ListView imports to DataSyncFragment

--- app/src/main/java/ug/go/health/ihrisbiometric/fragments/DataSyncFragment.java
+++ app/src/main/java/ug/go/health/ihrisbiometric/fragments/DataSyncFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
